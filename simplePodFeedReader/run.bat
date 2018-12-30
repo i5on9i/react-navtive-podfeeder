0f55262ee97de60cda7745619d4439a39570d1f7
i5on9i <gaedduck@gmail.com>
2018-12-30 17:36:14 +0900

    first upload

---
+++ simplePodFeedReader/run.bat
@@ -0,0 +1,7 @@
+
+@IF /I "%1"=="bundle" (
+	react-native bundle --platform android --dev false --entry-file index.js --bundle-output android\app\src\main\assets\index.android.bundle --assets-dest android\app\src\main\res
+) ELSE (
+	react-native bundle --platform android --dev false --entry-file index.js --bundle-output android\app\src\main\assets\index.android.bundle --assets-dest android\app\src\main\res & react-native run-android    
+)
+
